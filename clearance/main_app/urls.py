1774ae43c4c4c4fc16230d71f93244d3952e20ca
Nicholas Emmons <emmonsnicholas7@gmail.com>
2023-06-16 14:13:53 -0400

    First case added

--- clearance/main_app/urls.py
+++ clearance/main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('cases/', views.cases_index, name='index'),
 ]
