015c5b9eb1d9d84bc6d6c0383900586c4b30521f
Cherch <b.cherchiglia@gmail.com>
2023-06-16 12:15:43 -0400

    Home & About Created, Views, Routes

--- clearance/main_app/urls.py
+++ clearance/main_app/urls.py
@@ -1,9 +1,7 @@
-from django.contrib import admin
-# Add the include function to the import
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # '' represents the "starts with" path
-    path('', include('main_app.urls')),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about')
 ]
