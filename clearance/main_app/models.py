e400206ffb50a0073925b92d0de2c22f108d02e8
Cherch <b.cherchiglia@gmail.com>
2023-06-17 08:55:53 -0400

    Reporting Model created

--- clearance/main_app/models.py
+++ clearance/main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
-
 from django.urls import reverse
+
+REPORTS = (
+    ('I', 'Internet'),
+    ('N', 'Newspaper'),
+    ('P', 'Press Conference'),
+    ('T', 'Television'),
+)
 
 # Create your models here.
 class Case(models.Model):
@@ -17,3 +23,15 @@ class Case(models.Model):
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'case_id': self.id})
+
+class Reporting(models.Model):
+    date = models.DateField()
+    report = models.CharField(
+        max_length=1,
+        choices=REPORTS,
+        default=REPORTS[0][0]
+    )
+    case = models.ForeignKey(Case, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_report_display()} on {self.date}"
