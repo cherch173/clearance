82104d682a4505b65c24d15f037fb3df980f58ad
Cherch <b.cherchiglia@gmail.com>
2023-06-17 09:42:22 -0400

    admin created and reportings data crud

--- clearance/main_app/admin.py
+++ clearance/main_app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Case
+from .models import Case, Reporting
+
 # Register your models here.
 admin.site.register(Case)
+admin.site.register(Reporting)
