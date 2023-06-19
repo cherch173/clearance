066da9ec9b1e43a8a9abf67da667eaa6db0f62a6
Platin Syla <platinsyla06@gmail.com>
2023-06-19 11:13:32 +0200

    Added login url to go to cases

--- clearance/clearance/settings.py
+++ clearance/clearance/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+LOGIN_REDIRECT_URL = '/cases/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
